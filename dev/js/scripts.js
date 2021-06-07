0f27fe1e68404b607649aefdb506f067a2bc026c
Isabella Pedreira <63382876+IsabellaPedreira@users.noreply.github.com>
2021-06-07 13:06:19 -0400

    adding timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,3 +3,48 @@ import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin (DrawSVGPlugin); 
+
+
+  
+
+    //add tools
+    //GSDevTools.create();
+  
+    /* add your code here */
+    //Variables
+    let mainTL = gsap.timeline({id:"main"});
+
+
+    function init(){    
+
+
+
+
+    }
+
+    function fadeInTL(){
+        let tl = gsap.timeline();
+
+        return tl;
+
+    }
+
+    //1. set initial properties
+init();
+
+//2. show content - prevents FOUC
+gsap.set('#svg-container',{visibility:"visible"});
+
+//3. BUILD Main timeline
+mainTL.add(fadeInTL())
+.add(dashboardTL())
+.add(menuTL())
+.add(musicTL())
+
+
+
+;//tl END
+
+//mainTL.play("target");
+
+
